297a972b096f572aee46d3113d1f8f5e888dd5be
Will Yngson <christianwillyngson@gmail.com>
2021-01-19 23:15:51 +0800

    removed build folder from gitignore

---
+++ client/build/precache-manifest.7b3440fae558906d2115fd671c5c566e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5883ac5023d1cda7a6cd1ba602aeee4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "474e0904180e4196febb",
+    "url": "/static/css/main.d95a231d.chunk.css"
+  },
+  {
+    "revision": "84e6aa32433a6f39d1bd",
+    "url": "/static/js/2.afc69e79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.afc69e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474e0904180e4196febb",
+    "url": "/static/js/main.acf48031.chunk.js"
+  },
+  {
+    "revision": "0df8dd214d7feb922fa1",
+    "url": "/static/js/runtime-main.155aad04.js"
+  },
+  {
+    "revision": "87fef34d28ec37623a2cce8ef1cb171f",
+    "url": "/static/media/campfire.87fef34d.png"
+  },
+  {
+    "revision": "ea3759d62c8d20c732cc4cdf046d689a",
+    "url": "/static/media/class.ea3759d6.jpg"
+  }
+]);
