9132c2bab42ba106b58338d1d5fac2e6edf751b5
Will Yngson <christianwillyngson@gmail.com>
2021-01-29 14:03:58 +0800

    updated querying for user index

--- server/controllers/users/index.ts
+++ server/controllers/users/index.ts
@@ -2,12 +2,17 @@ module.exports = async (req, res) => {
     const {User} = require('../../models');
     try {
         const users = await User.findAll();
+        res.status(200).send({
+            message: 'Successfully retrieved all users',
+            other: 'hello',
+            users: users
+        })
     } catch(error) {
         console.log('the error for query is: ', error);
+        res.status(400).send({
+            message: 'There was an error retrieving the users',
+            error: error
+        })
     }
-    res.status(200).send({
-        message: 'Successfully retrieved all users',
-        other: 'hello',
-        users: users
-    })
+    
 }
