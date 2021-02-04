1a1a03b52dcf9c17cda82f777289bca11717ca32
Will Yngson <christianwillyngson@gmail.com>
2021-02-04 11:29:38 +0800

    updated model index js

--- server/models/index.js
+++ server/models/index.js
@@ -9,8 +9,8 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-    sequelize = new Sequelize(config.use_env_variable, config);
+if (config.DATABASE_URL) {
+    sequelize = new Sequelize(config.DATABASE_URL, config);
 //   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
